f547ed55156b5987f4ce7b4ad890ae5e8f0f835d
wendelca <vvkamalov_obs@mail.ru>
2025-04-18 14:44:19 +0500

    test: add getUserByEmail_UserExists test in UserServiceTest class

--- src/test/java/com/github/tennyros/dashboard/UserServiceTest.java
+++ src/test/java/com/github/tennyros/dashboard/UserServiceTest.java
@@ -1,68 +1,50 @@
-//package ru.kata.spring.boot;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import ru.kata.spring.boot.exceptions.RoleNotFoundException;
-//import ru.kata.spring.boot.models.Role;
-//import ru.kata.spring.boot.models.User;
-//import ru.kata.spring.boot.repositories.UserRepository;
-//import ru.kata.spring.boot.services.RoleService;
-//import ru.kata.spring.boot.services.impl.UserServiceImpl;
-//
-//import java.util.HashSet;
-//import java.util.Optional;
-//import java.util.Set;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//import static org.mockito.Mockito.when;
-//
-//@ExtendWith(MockitoExtension.class)
-//class UserServiceTest {
-//
-//    private static final Logger log = LoggerFactory.getLogger(UserServiceTest.class);
-//
-//    @Mock
-//    private UserRepository userRepository;
-//
-//    @InjectMocks
-//    private UserServiceImpl userService;
-//
-//    @BeforeEach
-//    public void setUp() {
-//       MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    void testGetUserByEmail_UserExists() {
-//        Long userId = 1L;
-//        String firstName = "firstName";
-//        String lastName = "lastName";
-//        String password = "password";
-//        int age = 20;
-//        String email = "admin@mail.com";
-//
-//        User user = new User();
-//        user.setId(userId);
-//        user.setFirstName(firstName);
-//        user.setLastName(lastName);
-//        user.setPassword(password);
-//        user.setAge(age);
-//        user.setEmail(email);
-//        log.info("User: {}", user);
-//        System.out.println(user);
-//        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
-//
-//        Optional<User> foundUser = userService.getUserByEmail(email);
-//
-//        assertTrue(foundUser.isPresent());
-//        assertEquals(email, foundUser.get().getUsername());
-//    }
-//}
+package com.github.tennyros.dashboard;
+
+import com.github.tennyros.dashboard.models.User;
+import com.github.tennyros.dashboard.repositories.UserRepository;
+import com.github.tennyros.dashboard.services.impl.UserServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Test
+    void testGetUserByEmail_UserExists() {
+
+        String email = "admin@mail.com";
+
+        User user = User.builder()
+                .id(1L)
+                .firstName("firstName")
+                .lastName("lastName")
+                .password("password")
+                .age(20)
+                .email(email)
+                .build();
+
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+
+        Optional<User> foundUser = userService.getUserByEmail(email);
+
+        assertTrue(foundUser.isPresent());
+        assertEquals(email, foundUser.get().getUsername());
+    }
+}
