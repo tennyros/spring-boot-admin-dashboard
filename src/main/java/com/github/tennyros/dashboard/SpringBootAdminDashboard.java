c2d79b0bcaa62ff5f58bd5ca834d3ae805ca8fe3
wendelca <vvkamalov_obs@mail.ru>
2025-04-18 17:36:47 +0500

    chore: change main class name

--- src/main/java/com/github/tennyros/dashboard/SpringBootAdminDashboard.java
+++ src/main/java/com/github/tennyros/dashboard/SpringBootAdminDashboard.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityDemoApplication {
+public class SpringBootAdminDashboard {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+        SpringApplication.run(SpringBootAdminDashboard.class, args);
     }
 }
